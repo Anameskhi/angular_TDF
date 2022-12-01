17b4867ba6662f15c107f68f4c7a848edcab8265
Anameskhi <ameskhi@unisens.ge>
2022-12-01 14:23:26 +0400

    before submit button work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+ 
   title = 'forms_angular';
+  topics = ['Angular', 'React', 'Vue']
+   topicHasError = true
+  userModel = new User('Rob','an@gm.com',571087888,"default","morning",true)
+
+  validateTopic(value: string){
+    if(value === "default"){
+      this.topicHasError = true
+    }else{
+      this.topicHasError = false
+    }
+
+  }
+  onSubmit(){
+
+  }
 }
